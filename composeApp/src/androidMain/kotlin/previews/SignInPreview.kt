d4be30003a9d6aee80ac2172e0f68d75cd1cbb40
christianGlez100 <christian.glez.100@gmail.com>
2025-04-19 20:50:02 -0600

    fix preview + koin

--- composeApp/src/androidMain/kotlin/previews/SignInPreview.kt
+++ composeApp/src/androidMain/kotlin/previews/SignInPreview.kt
@@ -3,12 +3,17 @@ package previews
 import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.sesi.quizly.di.appModule
 import com.sesi.quizly.ui.signin.SignInScreen
+import org.koin.core.context.startKoin
+import org.koin.dsl.koinApplication
 
 @Preview(showSystemUi = true, device = "id:pixel_7")
 @Composable
 fun LoginScreenPreview() {
-    Box {
-        SignInScreen()
+    startKoin  {
+        modules(appModule())
     }
+        SignInScreen()
+
 }
--- composeApp/src/commonMain/kotlin/com/sesi/quizly/App.kt
+++ composeApp/src/commonMain/kotlin/com/sesi/quizly/App.kt
@@ -8,7 +8,6 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 import org.koin.compose.KoinContext
 
 @Composable
-@Preview
 fun App() {
     PreComposeApp {
         KoinContext {
