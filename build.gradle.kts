ddd529fe443277a9ccc389de7030c62af378bba0
christianGlez100 <christian.glez.100@gmail.com>
2025-10-31 13:52:57 -0600

    sonar cloud test

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,23 +9,3 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform) apply false
     alias(libs.plugins.sonarqube)
 }
-/*
-allprojects {
-    apply(plugin = "org.sonarqube")
-}
-subprojects {
-
-    sonarqube {
-        properties {
-            property("sonar.projectKey", "christianGlez100_Quizly")
-            property("sonar.projectName", project.name)
-            property("sonar.language", "kotlin")
-            property("sonar.sourceEncoding", "UTF-8")
-            property("sonar.host.url", "https://sonarcloud.io")
-            property("sonar.organization","christianglez100")
-            property("sonar.login","81e0198c6dfc8ee2c1790af25c633350c1654bbb")
-            property("sonar.sources", "src/androidMain/kotlin,src/commonMain/kotlin,src/iosMain/kotlin,src/main/kotlin")
-            property("sonar.tests", "src/commonTest/kotlin,src/test/kotlin")
-        }
-    }
-}*/
